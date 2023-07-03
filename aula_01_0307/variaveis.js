b964ea5f66fb5afff39326760781a777fcfbbb33
RyanSMaciel <ryandossantosmaciel@gmail.com>
2023-07-03 12:04:12 -0300

    Iniciando com JS

---
+++ aula_01_0307/variaveis.js
@@ -0,0 +1,41 @@
+// JavaScript -> Linguagem de Programação (Interpretada)
+// Java -> Linguagem de Programação (Compilada)
+
+// Chrome -> V8 -> Interpretador do JS
+// Node.js -> V8 + NPM (Node Package Manager)
+
+// console.log("Texto")
+// console.log(5)
+
+//Variáveis -> Espaço (na memória) para armazenar um valor
+
+let nome                    // Declaração da variável nome
+nome = "Ryan Maciel";        // atribuição do valor a variável
+console.log(nome);           // Imprime na saída
+
+let idade = 23;
+console.log(idade);
+
+let altura = 1.75;
+console.log(altura);
+
+let cpf = "123.456.789-10";
+console.log(cpf);
+
+let casado = false;
+console.log(casado);
+
+nome = "Daniel Cardoso";
+console.log(nome);
+
+idade = 26;
+console.log(idade);
+
+const PI = 3.14;
+console.log(PI);
+
+// PI = 14      //GERA UM ERRO, POIS CONSTANTES NÃO PODEM SER ALTERADAS
+//console.log(PI)
+
+// Tipagem Estática -> Definição de um tipo a cada variável (java, C, C++, TypeScript)
+// Tipagem Dinâmica -> O tipo da variável é definido de acordo com seu valor (JavaScript, Python)
