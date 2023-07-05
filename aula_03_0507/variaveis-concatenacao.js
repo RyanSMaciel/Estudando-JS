f98eb1eb87fade271337123792b717b00c4296e7
RyanSMaciel <ryandossantosmaciel@gmail.com>
2023-07-05 12:08:59 -0300

    Condições e concatenação

---
+++ aula_03_0507/variaveis-concatenacao.js
@@ -0,0 +1,30 @@
+// let numero = 14,
+//     numero2 = 14,
+//     n23umero = 90,
+//     _ = 67,
+//     $ = 56;
+
+// var texto = "asdwasd",
+//     numero3 = 34;
+
+// Escopo Global
+
+// let num1 = 22;
+// console.log(num1);
+
+// console.log(num2);
+// var num2 = 24;
+
+// Operador de Concatenação +
+// const numero = 2 + 3; // Operador de adição
+// const texto = "O número é " + numero; // Operador de concatenação
+// console.log(texto);
+
+// console.log("A soma de " + 2 + " com " + 3 + " é igual a " + (2+3))
+
+let idade = 8;
+let mensagem = (idade >= 18) ? "Sua idade é " + idade + " pode ser preso! " : "Sua idade é " + idade + " NÃO pode ser preso! "
+// console.log(mensagem)
+
+const nome = "Ryan";
+console.log("Bem vindo,", nome, "você te muma reunião mais tarde!")
