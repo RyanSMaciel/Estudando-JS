0b0744426203b3159ad103fbd8efd1d559b69d4b
RyanSMaciel <ryandossantosmaciel@gmail.com>
2023-07-06 13:17:12 -0300

    finalizando

---
+++ aula_04_0607/repeticoes.js
@@ -0,0 +1,26 @@
+// for(let numero = 0 ; numero < 10; numero++){
+//     console.log(numero)
+// }
+// console.log("--------------------------------------")
+// for(let n1 = 0 ; n1 < 10; n1+= 2){
+//     console.log(n1)
+// }
+
+let n = 0
+while(n < 17){
+    console.log(n)
+    n++;
+}
+
+// for -> Uma quantidade X de vezes
+// while -> quantidade indeterminada de vezes
+
+let num = 80;
+let primo = false;
+for(){
+
+}
+
+if(primo){
+    console.log("É primo!!")
+}
