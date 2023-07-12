74ffaa68ed65072c70b36737bd56185c51ac6a91
RyanSMaciel <ryandossantosmaciel@gmail.com>
2023-07-12 13:43:24 -0300

    finalizando JS

--- aula_08_1207/funcoes-anonimas-arrows.js
+++ aula_08_1207/funcoes-anonimas-arrows.js
@@ -63,12 +63,39 @@
 //     console.log("Função Anônima");
 // }, 2000);
 
-setTimeout(() => {
-    console.log("setTimeout com Função Arrow")
-}, 2000);
+// setTimeout(() => {
+//     console.log("setTimeout com Função Arrow")
+// }, 2000);
 
-// parametro => escopo
-(resultado, requisicao) =>{
-    console.log(resultado + " " + requisicao)
-    return 0;
+// // parametro => escopo
+// (resultado, requisicao) =>{
+//     console.log(resultado + " " + requisicao)
+//     return 0;
+// }
+
+// testando a utilização das arrow functions
+function teste(callback) {
+    let valor = callback(130);
+    console.log(valor);
 }
+
+// Primeira forma de executar "teste" (Função Expression)
+function vezes2(numero) {
+    return (2 * numero);
+}
+teste(vezes2);
+
+// Segunda forma de executar "teste" (Função Anonima)
+teste(function(numero) {
+    return (numero * 2);
+});
+
+// Terceira forma de executar "teste" (Função Arrow) - Modo 2
+teste((numero) =>{
+    return (numero * 2);
+});
+
+// Quarta forma de executar "teste" (Função Arrow) - Modo 2
+teste(numero => (numero * 2));
+
+setInterval(() => console.log("Olá, Mundo"), 3000);
